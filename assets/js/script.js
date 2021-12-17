d4a74be2568857d81c9552632d22bf14d21d8006
TCL git <tuckerlarrabee13@gmail.com>
2021-12-17 09:11:41 -0500

    console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-window.alert("This is a TEST!!!!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+console.log(10+10);
+
+console.log("This is  a string!");
+
+console.log("Our robot's name is " + playerName);
+
+function fight () {
+    window.alert("The fight has begun!");
+}
+
+// This is a comment!!!
+
+// fight();
